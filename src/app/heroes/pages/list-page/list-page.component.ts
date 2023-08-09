1a6a40ca4b4664ae5ff6e3f4e24ea24f0d4eb534
Pochiwas <Pochiwas@gmail.com>
2023-08-09 16:38:47 -0400

    hero list and hero page integration

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-list-page',
@@ -6,6 +8,22 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+
+  public heroes :Hero[] =[]
+
+  constructor(private heroService:HeroesService){
+
+  }
+  //recordar que cuando se realiza se suscribe a un servicio es por que va a estar
+  //a la escucha de lo que suceda al evento al cual se esta suscribiendo
+  //en este caso al evento del servicio solicitado
+  ngOnInit(): void {
+   this.heroService.getHeroes()
+   .subscribe(heroes => this.heroes = heroes)
+   
+   console.log(this.heroes);
+  }
+
 
 }
