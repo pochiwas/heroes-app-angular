be4e25a031f23b228066338428b588204d2c91a1
Elias Yanez <pochito@TowerGames.localdomain>
2023-09-12 19:52:14 -0300

    realizacion crud

--- src/app/heroes/pipes/hero-image.pipe.ts
+++ src/app/heroes/pipes/hero-image.pipe.ts
@@ -7,12 +7,15 @@ import { Hero } from '../interfaces/hero.interface';
 export class HeroImagePipe implements PipeTransform {
 
   transform(hero:Hero): string {
-    
-    if (!hero.id && !hero.alt_img) {
-      return 'assets/no-image.png'
+
+    if ( !hero.id && !hero.alt_img ) {
+      return 'assets/no-image.png';
     }
 
-    return `assets/heroes/${hero.id}.jpg`
+    if ( hero.alt_img ) return hero.alt_img; // https:///google.com/flash.png
+
+    return `assets/heroes/${ hero.id }.jpg`;
+
   }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environment/envinronment';
 
@@ -23,7 +23,26 @@ export class HeroesService {
   }
 
   getSuggestion(query: string): Observable<Hero[]> {
-    
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw Error('Hero id is required');
+
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHeroById(id: string): Observable<boolean> {
+    
+
+    return this.http.delete<Hero>(`${this.baseUrl}/heroes/${id}`)
+    .pipe(
+      map(resp => true),
+      catchError(err => of(false))
+    );
   }
 }
