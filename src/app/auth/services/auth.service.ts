cff515471b2278e65e77b8e5dd1d6d3b9dc49b15
Elias Yañez <eyanez@bch.bancodechile.cl>
2024-03-12 12:32:46 -0300

    generacion de logica de login y logout

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,27 +1,48 @@
+import { User } from './../interfaces/user.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environments } from 'src/environment/envinronment';
-import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    private baseUrl = environments.baseUrl;
-    private user?: User;
+  private baseUrl = environments.baseUrl;
+  private user?: User;
 
-    get currentUser(): User | undefined {
-        if (!this.user) return undefined
-        return this.user
-    }
-    
-    login(username: string, password: string):Observable<User> {
-         
-      return  this.http.get<User>(this.baseUrl+'users/1')
-        .pipe(
-            tap( user => this.user = user),
-            tap( user => localStorage.setItem('token',user.id.toString()))
-        )
-    }
+  get currentUser(): User | undefined {
+    if (!this.user) return undefined
+    return this.user
+  }
+
+  login(email: string, password: string): Observable<User> {
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(user => this.user = user),
+        tap(user => localStorage.setItem('token', user.id.toString()))
+      )
+  }
+
+  checkAuthentication(): Observable<Boolean> | boolean {
+    //validacion de la existencia del token en el localstorage
+    if (!localStorage.getItem('token')) return false;
+    //obtener token desde localstorage
+    const token = localStorage.getItem('token');
+    //llamada del servicio de usuario
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+    //validacion de operadores de rxjs
+      .pipe(
+        tap(user => this.user = user),
+        map(user => !!user),
+        catchError(err => of(false))
+      )
+
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
+  }
 }
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { AuthService } from './../../../auth/services/auth.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page',
@@ -13,4 +16,16 @@ export class LayoutPageComponent {
     {label:'Agregar',icon:'add',url:'./new-hero'},
     {label:'Buscar',icon:'search',url:'./search'}
   ]
+
+  constructor(private authService:AuthService,
+              private router:Router){}
+
+  get user():User | undefined{
+    return this.authService.currentUser;
+  }
+
+  onLogout(){
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
 }
